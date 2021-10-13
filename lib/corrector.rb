aeb0a57fb88eb795bdb29e87eaf2b887e9d7a4b3
Diego Solis <disc3110@gmail.com>
2021-10-13 08:47:56 -0500

    Revert "add decoretor design pattern to validate name"
    This reverts commit cc246d507ba261c2d9125ecf53ed82ce753313ae.

--- lib/corrector.rb
+++
@@ -1,5 +0,0 @@
-class Corrector
-  def correct_name(text)
-    text.capitalize[0..9]
-  end
-end
--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,3 @@
-require_relative 'corrector'
-
 class Person
   attr_accessor :name, :age
   attr_reader :id
@@ -9,15 +7,10 @@ class Person
     @name = name
     @age = age
     @parent_permission = parent_permission
-    @corrector = Corrector.new
   end
 
   def can_use_services?
     of_age? || @parent_permission
-  end
-
-  def validate_name
-    @corrector.correct_name(@name)
   end
 
   private
