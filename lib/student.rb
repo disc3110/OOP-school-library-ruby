3607788b93dfe796261f429d618547eeb75fc193
Diego Solis <disc3110@gmail.com>
2021-10-13 09:58:19 -0500

    set up associations

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,15 @@
 class Student < Person
+  attr_reader :classroom
+
   def initialize(age, name, parent_permission, classroom: nil)
     @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
     super(age, name, parent_permission)
+  end
+
+  def classroom = (classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
